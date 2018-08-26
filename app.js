f49e23214e533bd3fb714103d0354084163d3aed
afmire877 <33981371+afmire877@users.noreply.github.com>
2018-08-26 17:06:17 +0100

    first Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+function updateClock(){
+	const now = new Date(); 
+	var hour = document.getElementById('hour');
+	var minute = document.getElementById('minute');
+	var seconds = document.getElementById('seconds');
+	hour.innerHTML = now.getHours();         
+	minute.innerHTML = now.getMinutes();       
+	seconds.innerHTML = now.getSeconds();   	
+
+}
+setInterval(updateClock, 1000);
