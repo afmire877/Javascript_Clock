82c3ae12ab2425358dd0e670ca490d8f28a77d02
afmire877 <33981371+afmire877@users.noreply.github.com>
2018-08-27 16:22:41 +0100

    Styled

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-
-
-
 function updateClock(){
 	const now = new Date(); 
 	var hour = document.getElementById('hour');
 	var minute = document.getElementById('minute');
 	var seconds = document.getElementById('seconds');
-	hour.innerHTML = now.getHours();         
+	hour.innerHTML =  now.getHours() ;         
 	minute.innerHTML = now.getMinutes();       
 	seconds.innerHTML = now.getSeconds();   	
 
